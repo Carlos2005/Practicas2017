c40503bf4e2f12eccee2c2e27222df88d6ad78f6
Carlos <Carlos2005@users.noreply.github.com>
2017-06-09 15:49:55 +0200

    Create practica_2.cpp

---
+++ practica_2.cpp
@@ -0,0 +1,19 @@
+/***********************************************
+ * NOMBRE: #Clark#
+ * PRIMER APELLIDO: #Kent#
+ * SEGUNDO APELLIDO: #Joconel#
+ * DNI: #99999999#
+ * EMAIL: #clark.kent@parida.net#
+***********************************************/
+
+
+#include<stdio.h>
+
+int main() {
+
+	printf("Clark\n");
+	printf("Kent\n");
+	printf("Joconel\n");
+	printf("99999999\n");
+	printf("clark.kent@parida.netk\n");
+}
